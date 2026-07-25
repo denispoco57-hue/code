de0a8147b6b307a7e6659b06ea9ae84720ba21a5
denispoco57-hue <denispoco57@example.com>
2026-07-25 14:24:04 +0300

    sdl3

---
+++ lib.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <string_view>
+#include <conio.h>
+#include <windows.h>
+
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
--- test.cpp
+++ test.cpp
@@ -1,84 +1,32 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <string_view>
-#include <conio.h>
-#include <windows.h>
-std::string g_ver = "0.01";
+#include "lib.hpp"
 
-void print_denis(){
-    std::cout << R"(                                                                                                
-                                          :}<~.                                          .:+]^.                                          
-                                          ]@%#}{{@[*:                              .-<{%{}}#@@=                                          
-                                         .#@@%##{}[[[}@}+.                     :<%%}[[[{{#%%@@>                                          
-                                          }@@@@@@%%##{}[[[{[-              .*{}[[}}{#%%%@@@@@@*                                          
-                                               .-+(%@@%%#{}[[{]:         =#}[}}##%%@@{>=:.                                               
-                                                      .>@@@%{[}@^       }#[}#@@@}=.                                                      
-                                                          .>@@%-         >@@#~                                                           
-                                                                                                                                         
-                                                                                                                                         
-                                                        =%@@@{~          .>%@@%>:                                                        
-                                                       {@@< .(@[.       :@@%= -{@^.                                                      
-                                                     :{@@@%<+<}@}       )@@@{*+(#@*                                                      
-                                                     =@@@@@@@@@@%~     .#@@@@@@@@@#:                                                     
-                                                     >@@@%%%%%%@@^     +@@@%%%%%@@@-                                                     
-                                                     ^@#}[[[[[}#@*     +@{}[[[[[}%@-                                                     
-                                                     -@#))))))<{{      .#}<))))<(%)                                                      
-                                                      =@#>**+^}@=       ^@[^*+*<%{.                                                      
-                                                       -{@@%%@{:         :[@@%@@<.                                                       
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         
-                                                                                      ~-.                                                
-                                                                                    .-+}@@>.                                             
-                                                                                   .*[@@@@@@<                                            
-                                                                            .~<{@@@%]<<){@@=^@-                                          
-                                                           .:-~~==+>)[%@@@@@#[)*+=~+^*>#@@[.  >(.                                        
-                                                           .~(@%]()<^+=~-:.    :<~.*[@@@@%:                                              
-                                                              -#@[:           .=<(>+^}[%@*                                               
-                                                                :[@@*.         .-=.=+]@%~                                                
-                                                                   -[@@%>~.      :>%@%+                                                  
-                                                                      .:*[@@@@@@@{)-                    )" << std::endl;
-}
+int main(int argc,char* argv[]){
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "sdl init video failed = " << SDL_GetError() << '\n';
+        return 1;
+    }
 
-void task_wait(int time, std::string type) {
-    // micro - microseconds,mili - miliseconds, sec - seconds, min - minutes
-    if (type == "micro") 
-        std::this_thread::sleep_for(std::chrono::microseconds(time));
-    else if(type == "mili") 
-        std::this_thread::sleep_for(std::chrono::milliseconds(time));
-    else if (type == "sec") 
-        std::this_thread::sleep_for(std::chrono::seconds(time));
-    else if (type == "min")
-        std::this_thread::sleep_for(std::chrono::minutes(time));
-    else 
-    std::cout << "bleh" << '\n';
-}
+    SDL_Window* window = SDL_CreateWindow("testing", 800,600,0);
 
-int main(){
-    std::cout << "Enter a username:" << '\n';
-    std::string user = "";
-    std::cin >> user;
-    if (user == "NeWt0nk") {
-        std::cout << "Enter passwords for this user:" << '\n';
-        std::string pass = "";
-        std::cin >> pass;
-        if (pass == "fahk1putinz0") {
-            std::cout << "Login completed, starting the loading sequence of ver " << g_ver <<'\n';
-            std::cout << "printing denis" << '\n';
-            print_denis();
-            task_wait(3,"sec");
-            // loading function or whatever
-        } else
-        std::cout << "Wrong password" << '\n';
-    } else 
-    std::cout << "No such user found" << '\n';
-    std::cout << std::endl;
+    if (!window) {
+        std::cerr << "window couldnt be created = " << SDL_GetError() << '\n';
+        SDL_Quit();
+        return 1;
+    }
+
+    bool run = true;
+    SDL_Event e;
+    while (run) {
+        while (e.type = SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) 
+                run = false;
+        }
+    }
+
+    // cleanup
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
-}
+}  
