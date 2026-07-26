c11b9a66385109bbc541a2757fa67f9ace53b5aa
denispoco57-hue <denispoco57@example.com>
2026-07-26 18:07:55 +0300

    bleeeh

--- lib.hpp
+++ lib.hpp
@@ -1,3 +1,6 @@
+// define
+#define nil nullptr
+//c++
 #include <iostream>
 #include <unordered_map>
 #include <string>
@@ -6,6 +9,7 @@
 #include <string_view>
 #include <conio.h>
 #include <windows.h>
-
+// sdl3
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
--- test.cpp
+++ test.cpp
@@ -1,15 +1,33 @@
 #include "lib.hpp"
 
+
 int main(int argc,char* argv[]){
+    
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "sdl init video failed = " << SDL_GetError() << '\n';
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("testing", 800,600,SDL_WINDOW_BORDERLESS);
-
+    SDL_Window* window = SDL_CreateWindow("denis_sec", 800,600,SDL_WINDOW_BORDERLESS);
     if (!window) {
         std::cerr << "window couldnt be created = " << SDL_GetError() << '\n';
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nil);
+    if (!renderer) {
+        std::cerr << "couldnt make a renderer = " << SDL_GetError() << '\n';
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture* image = IMG_LoadTexture(renderer,"D:/vs/code/test.jpg");
+    if (!image) {
+        std::cerr << "couldnt load image = " << SDL_GetError() << '\n';
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
         SDL_Quit();
         return 1;
     }
@@ -18,15 +36,21 @@ int main(int argc,char* argv[]){
     SDL_Event e;
     while (run) {
         while (e.type = SDL_PollEvent(&e)) {
-            if (e.type == SDL_EVENT_QUIT) 
-                run = false;
+            if (e.type == SDL_EVENT_QUIT) run = false; break;
+
+            SDL_SetRenderDrawColor(renderer,30, 30, 40, 255);
+            SDL_RenderClear(renderer);
+            SDL_RenderTexture(renderer,image,nil,nil);
+            SDL_RenderPresent(renderer);
         }
     }
 
     // cleanup
 
     SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(image);
     SDL_Quit();
-
+    std::cout<<std::endl;
     return 0;
 }  
